68950cd6addae7440228ccf632184b589bc6645e
AlexT <aletrus@yandex.ru>
2019-02-28 00:07:12 +0300

    выполнено задание урока 3. в дробях get/set на чтение не сделано. задание 2 в столбик не выводится. доработать

---
+++ Lesson_3/Drob/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drob
+{
+
+    class Program
+    {
+        class Drob
+        {
+            private int numer; //числитель
+            private int denom; //знаменатель
+
+            public Drob(int numer, int denom) // конструтор с проверкой на 0 в знаменателе
+            {
+                if (denom == 0)
+                {
+                    throw new ArgumentException("В знаменателе не может быть нуля");
+                }
+                this.numer = Math.Abs(numer);
+                this.denom = Math.Abs(denom);
+                }
+
+            public override string ToString() // строковое представление для вывода на экран
+            {
+                return numer.ToString() + "/" + denom.ToString(); 
+            }
+
+            public static Drob SetFormat(Drob a) // сокращение дроби и получение знака дроби
+            {
+                int max = 0;
+                if (a.numer > a.denom)
+                {
+                    max = Math.Abs(a.denom);
+                }
+                else
+                {
+                    max = Math.Abs(a.numer);
+                }
+                for (int i = max; i < 2; i--)
+                {
+                    if ((a.numer % i == 0) && (a.denom % i == 0))
+                    {
+                        a.numer = a.numer / i;
+                        a.denom = a.denom / i;
+                    }
+                }
+                if (a.denom < 0)
+                {
+                    a.numer = -1 * (a.numer);
+                    a.denom = Math.Abs(a.denom);
+                }
+                return a;
+            }
+
+            public static Drob operator +(Drob a, Drob b)// сложение дробей
+            {
+                Drob t = new Drob(1, 1);
+                t.numer = a.numer * b.denom + a.denom * b.numer;
+                t.denom = a.denom * b.denom;
+                SetFormat(t);
+                return t;
+            }
+
+            public static Drob operator -(Drob a, Drob b)// вычитание дробей
+            {
+                Drob t = new Drob(1, 1);
+                t.numer = a.numer * b.denom - a.denom * b.numer;
+                t.denom = a.denom * b.denom;
+                SetFormat(t);
+                return t;
+            }
+
+            public static Drob operator *(Drob a, Drob b)// умножение дробей
+            {
+                Drob t = new Drob(1, 1);
+                t.numer = a.numer * b.denom;
+                t.denom = a.denom * b.numer;
+                SetFormat(t);
+                return t;
+            }
+
+            public static Drob operator /(Drob a, Drob b)// деление дробей
+            {
+                Drob t = new Drob(1, 1);
+                t.numer = a.numer * b.denom;
+                t.denom = a.denom * b.numer;
+                SetFormat(t);
+                return t;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числитель первой дроби");
+            int c1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знаменатель первой дроби");
+            int z1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Введите знак действия с дробями");
+            //char mod = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите числитель второй дроби");
+            int c2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знаменатель второй дроби");
+            int z2 = Convert.ToInt32(Console.ReadLine());
+
+            Drob dr1 = new Drob(c1, z1);
+            Drob dr2 = new Drob(c2, z2);
+
+            Drob res = dr1 / dr2;
+            Drob res1 = dr1 * dr2;
+            Drob res2 = dr1 - dr2;
+            Drob res3 = dr1 + dr2;
+
+            Console.WriteLine($"Результат сложения дробей:\n" + dr1.ToString() + " + " + dr2.ToString() + " = " + res3.ToString());
+            Console.WriteLine($"Результат деления дробей:\n" + dr1.ToString() + " / " + dr2.ToString() + " = " + res.ToString());
+            Console.WriteLine($"Результат умножения дробей:\n" + dr1.ToString() + " * " + dr2.ToString() + " = " + res1.ToString());
+            Console.WriteLine($"Результат вычитания дробей:\n" + dr1.ToString() + " - " + dr2.ToString() + " = " + res2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_3/SumODD/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Трус Алексей
+//    С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+//Требуется подсчитать сумму всех нечётных положительных чисел.Сами числа и сумму
+//вывести на экран, используя tryParse.
+
+namespace SumODD
+{
+    class Program
+    {
+        private static int NewMethod(string message, out int x)
+        {
+            string s;
+            bool flag; 
+            do
+            {
+                Console.WriteLine(message);
+                s = Console.ReadLine(); // Если перевод произошел неправильно, то результатом будет false , т.е. 0;
+
+                flag = int.TryParse(s, out x);
+            }
+            while (!flag);
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            string message = "Введите число:";
+            
+                int x;
+                int summ = 0;
+
+            while (NewMethod(message, out x) != 0)
+            {
+                
+                if (x > 0 && x % 2 != 0)
+                    {
+                    summ = summ + x;
+                    Console.WriteLine("Сумма введенных нечётных положительных чисел " + summ);
+                    Console.WriteLine("Нечётное положительное число " + x);
+                    }
+                }
+            Console.ReadKey();
+        }
+    }
+}
+
